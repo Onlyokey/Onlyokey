c4bbad9a092ca37494ffe6ffd1434fd4c7cc9edc
Ustinsky Ilya <onlyokeey@gmail.com>
2016-12-08 21:00:30 +0200

    first save

---
+++ src/com/hillel/variables/Vars.java
@@ -0,0 +1,20 @@
+package com.hillel.variables;
+
+
+public class Vars {
+public static void main(String[] args){
+
+//byte var =127;
+//int num = var +1;
+//byte another = (byte) num;
+//System.out.println(another);
+float var = 3.5f;
+double d = 34.45d;
+double a =30;
+double b =25;
+double c = a/b;
+
+System.out.println(c);
+
+}
+}
